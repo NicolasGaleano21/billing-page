32a3367f6d83e2cb8ad89bea88da5823fa05d9f8
NicolasGaleano21 <niicogaleano4@gmail.com>
2025-08-19 12:53:14 -0300

    project startup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { CssBaseline, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Typography>Hello World!</Typography>
+    </>
+  );
+}
+
+export default App;
